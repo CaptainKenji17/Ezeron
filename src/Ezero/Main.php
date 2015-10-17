c367e1b975b578343139b52eae1a55d558c76a28
Shimamura Kenji <kenjitheextreme@gmail.com>
2015-10-18 03:25:29 +0900

    Update Main.php

--- src/Ezero/Main.php
+++ src/Ezero/Main.php
@@ -1,18 +1,19 @@
 <?php
-
 namespace Ezero;
+
 
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
-use pocketmine\level\sound\AnvilSound;
+use pocketmine\level\sound\AnvilFallSound;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\entity\Effect;
 use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\item\Item;
-use pocketmine\level\particle\CriticalParticle;
+use pocketmine\level\particle\DustParticle;
+use pocketmine\math\Vector3;
 
 class Main extends PluginBase implements Listener {
     
@@ -27,18 +28,18 @@ $p = $ev->getEntity();
             $damager = $ev->getDamager();
             if($damager instanceof Player){
                 if($damager->getInventory()->getItemInHand()->getId() === 369){
-                    $ev->setKnockBack(0.9);
-                    $ev->setDamage($ev->getDamage()+5);
+                    $ev->setKnockBack(0.5);
+                    $ev->setDamage($ev->getDamage()+3);
 $x = $p->getX();
 $y = $p->getY();
 $z = $p->getZ();
 $r = mt_rand(0, 255);
         $g = mt_rand(0, 255);
         $b = mt_rand(0, 255);
-        $p->getLevel()->addParticle(new CriticalParticle(new Vector3($x, $y, $z), $r, $g, $b));             
-                    $p->addEffect(Effect::getEffect(9)->setAmplifier(1)->setDuration(20)->setVisible(true));
+        $p->getLevel()->addParticle(new DustParticle(new Vector3($x, $y, $z), $r, $g, $b));       
+ $p->addEffect(Effect::getEffect(9)->setAmplifier(1)->setDuration(20)->setVisible(true));
                     $level = $damager->getLevel();
-                    $level->addSound(new AnvilSound($damager->getLocation()));
+                    $damager->getLevel()->addSound(new AnvilFallSound($damager->getLocation()));
                 }
             }
         }
@@ -47,9 +48,9 @@ $r = mt_rand(0, 255);
 $player = $ev->getPlayer();
         if($ev->getPlayer()->getInventory()->getItemInHand()->getId() === 369){
             $ev->getPlayer()->sendTip(TextFormat::RED."Ezero Enabled!");
-            $player->addEffect(Effect::getEffect(8)->setAmplifier(3)->setDuration(999999)->setVisible(true));
+            $player->addEffect(Effect::getEffect(11)->setAmplifier(0)->setDuration(999999)->setVisible(false));
         }else{
-            $player->removeAllEffects(); 
+            $player->removeEffect(11);
         }
     }
 }
