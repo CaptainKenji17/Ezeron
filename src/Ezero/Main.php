3ced78f2b767f6a5a9b3f6800fe3efc496e49e21
Shimamura Kenji <kenjitheextreme@gmail.com>
2016-02-20 17:45:41 +0900

    Maybe fix
    Not tested idk?

--- src/Ezero/Main.php
+++ src/Ezero/Main.php
@@ -23,29 +23,31 @@ class Main extends PluginBase implements Listener {
     }
     
     public function onHurt(EntityDamageEvent $ev){
-$p = $ev->getEntity();
+        $p = $ev->getEntity();
         if($ev instanceof EntityDamageByEntityEvent){
             $damager = $ev->getDamager();
             if($damager instanceof Player){
                 if($damager->getInventory()->getItemInHand()->getId() === 369){
                     $ev->setKnockBack(0.5);
                     $ev->setDamage($ev->getDamage()+3);
-$x = $p->getX();
-$y = $p->getY();
-$z = $p->getZ();
-$r = mt_rand(0, 255);
+      $x = $p->getX();
+      $y = $p->getY();
+      $z = $p->getZ();
+        $r = mt_rand(0, 255);
         $g = mt_rand(0, 255);
         $b = mt_rand(0, 255);
-        $p->getLevel()->addParticle(new DustParticle(new Vector3($x, $y, $z), $r, $g, $b));       
+        $cen =  new Vector3($x, $y, $z);    
+        $particle = new DustParticle($cen, $r, $g, $b);
+      $p->getLevel()addParticle($particle);
  $p->addEffect(Effect::getEffect(9)->setAmplifier(1)->setDuration(20)->setVisible(true));
                     $level = $damager->getLevel();
-                    $damager->getLevel()->addSound(new AnvilFallSound($damager->getLocation()));
+                    $level->addSound(new AnvilFallSound($damager->getLocation()),[$level]);
                 }
             }
         }
     }
     public function onItemHeld(PlayerItemHeldEvent $ev){
-$player = $ev->getPlayer();
+        $player = $ev->getPlayer();
         if($ev->getPlayer()->getInventory()->getItemInHand()->getId() === 369){
             $ev->getPlayer()->sendTip(TextFormat::RED."Ezero Enabled!");
             $player->addEffect(Effect::getEffect(11)->setAmplifier(0)->setDuration(999999)->setVisible(false));
